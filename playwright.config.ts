ad32cca48656c21c8efa8db0998f10d859430281
fainnerramirez <fainnerramirez@gmail.com>
2025-06-12 09:52:56 -0500

    feat: prueba home => payment

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 800000,
+  timeout: 500000,
   reporter: 'html',
   outputDir: 'test-results',
   use: {
